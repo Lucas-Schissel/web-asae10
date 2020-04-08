dd4de8546b2497819a493a132729f58813a6dc77
Cliente <Cliente@CLIENTE-PC>
2020-04-08 10:30:55 -0300

    4º envio

--- resources/views/tela_login.blade.php
+++ resources/views/tela_login.blade.php
@@ -4,17 +4,36 @@
 	<title>Login</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{asset('css/t1.css')}}" type="text/css">
+
 </head>
 <body>
 
+	
+	<div class="container mt-4 text-center">
+		<h1>Login</h1>
+		<div class="row">
+			<div class="col-md-2 mt-2">
+				<!-- coluna vazia esquerda -->
+			</div>
+			<div class="col-md-8 mt-3">		
+			
+			<form method="post" action="{{ route('logar') }}">
+				@csrf
+				<input type="text" name="login" placeholder="Login...">
+				<br><br>
+				<input type="password" name="senha" placeholder="Senha...">
+				<br><br>
+				<input type="submit"  value="Logar">
+			</form>
 
-	<h1>Login</h1>
-	<form method="post" action="{{ route('logar') }}">
-		@csrf
-		<input type="text" name="login" placeholder="Login">
-		<input type="password" name="senha" placeholder="Senha">
-		<input type="submit" value="Logar">
-	</form>
+			</div>
+			<div class="col-md-2 mt-2">
+				<!-- coluna vazia direita -->
+			</div>
+		</div>
+		
+	</div>
 
 
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
