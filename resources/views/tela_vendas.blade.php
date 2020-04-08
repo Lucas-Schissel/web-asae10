abfa14265a5fa93c68acdba6755e1910ea5f1236
Cliente <Cliente@CLIENTE-PC>
2020-04-08 19:21:00 -0300

    9º envio

--- resources/views/tela_vendas.blade.php
+++ resources/views/tela_vendas.blade.php
@@ -4,8 +4,9 @@
 <div class="text-center">
 	<h1>Venda para Clientes</h1>
 	<form method="post" action="{{ route('venda_add') }}">
-        @csrf
-        <input type="text" class="form-control" name="nome" placeholder="Nome">
+		@csrf
+		
+       <input type="text" class="form-control" name="nome" placeholder="Nome">
 		<br>
 		<input type="text" class="form-control" name="valor" placeholder="Valor">
 		<br>
